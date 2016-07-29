66278b612e0867bead66e323974637f4615be354
jendiamond <thejendiamond@gmail.com>
2016-07-28 23:57:59 -0700

    Generate Devise Model
    On branch devise
     Changes to be committed:
     new file:   app/models/model.rb
     modified:   app/models/user.rb
     modified:   config/routes.rb
     new file:   db/migrate/20160729064307_devise_create_models.rb
     modified:   db/schema.rb
     new file:   test/factories/models.rb
     new file:   test/models/model_test.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  protect_from_forgery prepend: true
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   get 'static_pages/index'
   get 'static_pages/about'
   get 'static_pages/faq'
